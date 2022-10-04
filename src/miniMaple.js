f8c2c96a67efb24582caa0afc6631416388204f7
local user <user@mil8a-118a-04.mmcs.sfedu.ru>
2022-10-04 19:05:04 +0300

    расклеить и склеить done

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,5 +1,29 @@
 class MiniMaple{
 
+
+
+
+}
+
+
+function spliter()
+{
+ const stringToSplit = "2+3+4+9-7-5";
+ const arrayOfOperands = stringToSplit.split(/[+-]/g);
+ arrayOfOperands.forEach(element => console.log(element));
+
+ const arrayOfOperators = stringToSplit.split(/[^+-]/g);
+ arrayOfOperators.pop();
+ arrayOfOperators.forEach(element => console.log(element));
+
+  let res = "";
+  for(let i=0; i < arrayOfOperators.length; i++)
+  {
+    res+= arrayOfOperators[i] + arrayOfOperands[i];
+  }
+  console.log(res);
+
 }
 
 export {MiniMaple}
+export {spliter}
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,78 @@
 import {MiniMaple} from "../src/miniMaple";
+import {spliter} from "../src/miniMaple";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+
+test('0', () => {
+    spliter();
 });
+
+/*
+//!!!!!
+//низя x*y
+//зя x+y
+
+test('1', () => {
+    expect(diff("4*x^3", "x")).toBeEqual("12*x^2");
+});
+
+test('2', () => {
+    expect(diff("4*x^3", "y")).toBeEqual("0");
+});
+
+test('3', () => {
+    expect(diff("4*x^3-x^2", "x")).toBeEqual("12*x^2-2*x");
+});
+//============================================================================
+test('4', () => {
+    expect(diff("4*x", "x")).toBeEqual("4");
+});
+
+test('5', () => {
+    expect(diff("4*x", "y")).toBeEqual("0");
+});
+
+test('6', () => {
+    expect(diff("4*x^3-x^2-8*x^6", "x")).toBeEqual("12*x^2-2*x-48*x^5");
+});
+
+test('7', () => {
+    expect(diff("4*x^3+x^2+8*x^6", "x")).toBeEqual("12*x^2+2*x+48*x^5");
+});
+
+test('8', () => {
+    expect(diff("4*x^3-x^2+8*x^6", "x")).toBeEqual("12*x^2-2*x+48*x^5");
+});
+
+test('9', () => {
+    expect(diff("4*x^3*y-x^2", "x")).toBeEqual("12*x^2*y-2*x");
+});
+
+test('10', () => {
+    expect(diff("4*x^3*y-x^2", "x")).toBeEqual("12*x^2*y-2*x");
+});
+
+test('11', () => {
+    expect(diff("4*x^3*y-x^2", "y")).toBeEqual("12*x^3");
+});
+
+test('12', () => {
+    expect(diff("y", "y")).toBeEqual("1");
+});
+
+test('13', () => {
+    expect(diff("45", "y")).toBeEqual("0");
+});
+
+test('14', () => {
+    expect(diff("45+98", "y")).toBeEqual("0");
+});
+
+test('15', () => {
+    expect(diff("45*x+98", "y")).toBeEqual("0");
+});
+
+test('16', () => {
+    expect(diff("-4*x^3", "x")).toBeEqual("-12*x^2");
+});
+
+*/
