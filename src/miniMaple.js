7aaf2899af5a4e180d5e9fe26033245e0b314727
Kondrateva Aelina <aelina.kondrateva@mail.ru>
2022-10-13 20:07:42 +0300

    back done

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,29 +1,117 @@
 class MiniMaple{
 
-
-
-
 }
 
 
-function spliter()
+function split(stringToSplit)
 {
- const stringToSplit = "2+3+4+9-7-5";
- const arrayOfOperands = stringToSplit.split(/[+-]/g);
- arrayOfOperands.forEach(element => console.log(element));
+    //stringToSplit = "4*x^2+3";
+    //stringToSplit = "4+3+6-8";
+    const arrayOfOperands = stringToSplit.split(/[+-]/);
+    arrayOfOperands.forEach(element => console.log("element:", element));
 
- const arrayOfOperators = stringToSplit.split(/[^+-]/g);
- arrayOfOperators.pop();
- arrayOfOperators.forEach(element => console.log(element));
+    const arrayOfOperators1 = stringToSplit.split(/[^+-]/g);
+    let arrayOfOperators = new Array();
+    if(arrayOfOperators1[0] != "-")
+        arrayOfOperators.push("");
+    arrayOfOperators1.forEach(element => {if(element != "") arrayOfOperators.push(element)});
+    arrayOfOperators.forEach(element => console.log("znak: ", element));
 
-  let res = "";
-  for(let i=0; i < arrayOfOperators.length; i++)
-  {
-    res+= arrayOfOperators[i] + arrayOfOperands[i];
-  }
-  console.log(res);
-
+    let res = "";
+    for(let i=0; i < arrayOfOperators.length; i++)
+    {
+        res+= arrayOfOperators[i] + arrayOfOperands[i];
+    }
+    return res;
 }
+
+function diff(stringToSplit, dx)
+{
+    //stringToSplit = "4*x^2+3";
+    //stringToSplit = '4+3+6-8';
+    //console.log(stringToSplit);
+    let f = 0;
+    if (stringToSplit[0] == "-")
+    {
+        stringToSplit = stringToSplit.slice(1, );
+        f = 1;
+    }
+
+    const arrayOfOperands = stringToSplit.split(/[+-]/g);
+    arrayOfOperands.forEach(element => console.log("elem: ", element));
+
+    const arrayOfOperators1 = stringToSplit.split(/[^+-]/g);
+    let arrayOfOperators = new Array();
+    if(arrayOfOperators1[0] != "-")
+        arrayOfOperators.push("");
+    arrayOfOperators1.forEach(element => {if(element != "") arrayOfOperators.push(element)});
+    arrayOfOperators.forEach(element => console.log("znak: ", element));
+
+    let arrayOfDiffs = new Array();
+    for(let i=0; i<arrayOfOperands.length;i++)
+    {
+        //console.log(arrayOfOperands[i]);
+        const l = simple_diff(arrayOfOperands[i], dx);
+        arrayOfDiffs.push(l);
+        //console.log(l);
+    }
+
+    let res = "";
+    if (f)  res = "-";
+    for(let i=0; i < arrayOfOperators.length; i++)
+    {
+        if(arrayOfDiffs[i] != 0)
+            res+= arrayOfOperators[i] + arrayOfDiffs[i];
+        //console.log("Operator", arrayOfOperators[i]);
+        //console.log("Operand", arrayOfOperands[i]);
+    }
+    if (res == "" || res == "-") res = "0";
+    return res;
+}
+
+function simple_diff(stringToDiff, dx) //x = dx, y != dx, number
+{
+    //console.log(stringToDiff, "(",dx,") =>",)
+    if(!stringToDiff.includes((dx))) return 0;
+    const arrayOfOperands = stringToDiff.split(/[*]/g);
+    let stepen = 0;
+    let coef = 1;
+    for(let i=0; i < arrayOfOperands.length; i++)
+    {
+        //console.log("element: ", arrayOfOperands[i]);
+        const c = Number(arrayOfOperands[i]);
+        if(!isNaN(c))//число
+            coef = c;
+        else//не число
+    {
+        if (arrayOfOperands[i].includes(dx))//по этому дифференцируем
+        {
+            const ar = arrayOfOperands[i].split('^');
+            if(ar.length == 1) stepen = 1;
+            else
+            {
+                for (let j = 0; j < ar.length; j++)
+                {
+                    if (ar[j] != dx)
+                    stepen = Number(ar[j]);
+                }
+            }
+        }
+    }
+    }
+    //console.log("coef: ", coef);
+    //console.log("stepen: ", stepen);
+    let res = (coef*stepen).toString();
+    if(stepen-1 == 1)  res += "*x";
+    else if(stepen-1 > 1)
+        res += "*x^" + (stepen-1).toString();
+
+    //console.log ("res: ",res);
+    return res;
+}
+
 
 export {MiniMaple}
-export {spliter}
+export {diff}
+export {simple_diff}
+export {split}
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,78 +1,83 @@
 import {MiniMaple} from "../src/miniMaple";
-import {spliter} from "../src/miniMaple";
+import {diff} from "../src/miniMaple";
+import {simple_diff} from "../src/miniMaple";
+import {split} from "../src/miniMaple";
+import expect from "expect";
 
 
-test('0', () => {
-    spliter();
-});
 
-/*
+
 //!!!!!
 //низя x*y
 //зя x+y
 
+
 test('1', () => {
-    expect(diff("4*x^3", "x")).toBeEqual("12*x^2");
+    expect(diff("4*x^3", "x")).toEqual("12*x^2");
+    //expect(split("4*x^3+3")).toEqual("4*x^3+3");
+    //diff("4*x^3", "x");
+    //split("4*x^3+3");
+    //split("43+3-39-6+63");
 });
 
 test('2', () => {
-    expect(diff("4*x^3", "y")).toBeEqual("0");
+    expect(diff("4*x^3", "y")).toEqual("0");
 });
 
 test('3', () => {
-    expect(diff("4*x^3-x^2", "x")).toBeEqual("12*x^2-2*x");
+    expect(diff("4*x^3-x^2", "x")).toEqual("12*x^2-2*x");
 });
 //============================================================================
 test('4', () => {
-    expect(diff("4*x", "x")).toBeEqual("4");
+    expect(diff("4*x", "x")).toEqual("4");
 });
 
 test('5', () => {
-    expect(diff("4*x", "y")).toBeEqual("0");
+    expect(diff("4*x", "y")).toEqual("0");
 });
 
 test('6', () => {
-    expect(diff("4*x^3-x^2-8*x^6", "x")).toBeEqual("12*x^2-2*x-48*x^5");
+    expect(diff("4*x^3-x^2-8*x^6", "x")).toEqual("12*x^2-2*x-48*x^5");
 });
 
 test('7', () => {
-    expect(diff("4*x^3+x^2+8*x^6", "x")).toBeEqual("12*x^2+2*x+48*x^5");
+    expect(diff("4*x^3+x^2+8*x^6", "x")).toEqual("12*x^2+2*x+48*x^5");
 });
 
 test('8', () => {
-    expect(diff("4*x^3-x^2+8*x^6", "x")).toBeEqual("12*x^2-2*x+48*x^5");
-});
-
-test('9', () => {
-    expect(diff("4*x^3*y-x^2", "x")).toBeEqual("12*x^2*y-2*x");
-});
-
-test('10', () => {
-    expect(diff("4*x^3*y-x^2", "x")).toBeEqual("12*x^2*y-2*x");
-});
-
-test('11', () => {
-    expect(diff("4*x^3*y-x^2", "y")).toBeEqual("12*x^3");
+    expect(diff("4*x^3-x^2+8*x^6", "x")).toEqual("12*x^2-2*x+48*x^5");
 });
 
 test('12', () => {
-    expect(diff("y", "y")).toBeEqual("1");
+    expect(diff("y", "y")).toEqual("1");
 });
 
 test('13', () => {
-    expect(diff("45", "y")).toBeEqual("0");
+    expect(diff("45", "y")).toEqual("0");
 });
 
 test('14', () => {
-    expect(diff("45+98", "y")).toBeEqual("0");
+    expect(diff("45+98", "y")).toEqual("0");
 });
 
 test('15', () => {
-    expect(diff("45*x+98", "y")).toBeEqual("0");
+    expect(diff("45*x+98", "y")).toEqual("0");
 });
 
 test('16', () => {
-    expect(diff("-4*x^3", "x")).toBeEqual("-12*x^2");
+    expect(diff("-4*x^3", "x")).toEqual("-12*x^2");
 });
 
-*/
+test('17', () => {
+    expect(diff("-4*x^3+2*x", "x")).toEqual("-12*x^2+2");
+});
+
+
+test('18', () => {
+    expect(diff("-4*x", "x")).toEqual("-4");
+});
+
+
+test('19', () => {
+    expect(diff("-4", "x")).toEqual("0");
+});
