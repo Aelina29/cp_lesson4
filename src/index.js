381a9c7b94258268a838d60d45eafe753ece339b
Kondrateva Aelina <aelina.kondrateva@mail.ru>
2022-10-15 16:06:38 +0300

    DONE!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+import {diff} from "./miniMaple";
+
 document.addEventListener('DOMContentLoaded',setup)
 
 function setup() {
-    document.getElementById('demoButton').onclick = addSomething;
+    document.getElementById('diffButton').onclick = differentiate;
 }
 
-function addSomething(){
+function differentiate() {
+    const str = document.getElementById('str').value;
+    const x = document.getElementById('x').value;
+    const res = diff(str, x);
+
     const someDummyDiv = document.createElement('div');
-    someDummyDiv.classList.add('generated');
-    const count = document.getElementsByClassName('generated').length;
-    someDummyDiv.innerHTML = `I was created by JS! There are already ${count} of my friends!`;
+    if (res == "NaN")
+        someDummyDiv.innerHTML = `${str}, ${x} => error!`;
+    else
+        someDummyDiv.innerHTML = `${str}, ${x} => ${res}`;
     const container = document.getElementById('container');
     container.appendChild(someDummyDiv);
 }
+
+    // const someDummyDiv = document.createElement('div');
+    // someDummyDiv.classList.add('generated');
+    // const count = document.getElementsByClassName('generated').length;
+    // someDummyDiv.innerHTML = `I was created by JS! There are already ${count} of my friends!`;
+    // const container = document.getElementById('container');
+    // container.appendChild(someDummyDiv);
